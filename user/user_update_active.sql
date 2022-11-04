014375ba5ea2e8c5b373f27eea0a974201f68321
Semenov <semenov@mail.ru>
2022-11-04 13:05:26 +0400

    update function user_update_active

--- user/user_update_active.sql
+++ user/user_update_active.sql
@@ -9,7 +9,7 @@ AS $function$
 	declare
 		user_ user_check_active;
 	BEGIN
-       select u.id, u.active into user_  from public.user_get_active(_id_user);
+       select * into user_  from public.user_get_active(_id_user);
 	   select * into result_type_ from public.user_check_id(user_);
 		if user_.active = true then
 			select 0 as status_, 'Пользователь уже активен для использования' as error_ into result_type_ ;
@@ -17,7 +17,7 @@ AS $function$
     	end if;
 	   if result_type_.status_ <> 0 then 
 	   	    update public."user" set active = true;
-			select 1 as status_, "" as error_ into result_type_ ;
+			select 1 as status_, null as error_ into result_type_ ;
     		PERFORM public.log_insert(_id_user, 'Пользователь успешно активирован');
 		end if;
 	END;
