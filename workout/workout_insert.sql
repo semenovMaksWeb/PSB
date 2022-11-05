87284a31c7d89de61daf472a5ed4e5d303a62c89
Semenov <semenov@mail.ru>
2022-11-05 16:31:55 +0400

    update workout_insert function

--- workout/workout_insert.sql
+++ workout/workout_insert.sql
@@ -2,27 +2,21 @@
 drop function public.workout_insert;
 /* drop function */
 
-CREATE OR REPLACE FUNCTION public.workout_insert(
-	_id_user int4, 
-	_date timestamp = CURRENT_TIMESTAMP, 
-	out id_ int,
-	out result_type_ result_type
-	)
-	RETURNS record
-	LANGUAGE plpgsql
+ CREATE OR REPLACE FUNCTION public.workout_insert(_id_user integer, _date timestamp without time zone DEFAULT CURRENT_TIMESTAMP, OUT id_ integer, OUT result_type_ result_type)
+ RETURNS record
+ LANGUAGE plpgsql
 AS $function$
-	declare
-		user_ user_check_active;
 	BEGIN
-		select * into user_  from public.user_get_active(_id_user);
-		select * into result_type_ from public.user_check_id(user_);
+		select * into result_type_  from check_user(_id_user);
 		if result_type_.status_ <> 0 then 
 			insert into workout (id_user, "date") values (_id_user, _date) RETURNING id into id_;
 			select 1 as status_, null as error_ into result_type_ ;
 			PERFORM public.log_insert(_id_user, 'Пользователь успешно создал тренировку id - ' || id_);
 		end if;
 	END;
-$function$;
+$function$
+;
+
 
 /* start function */
 select * from public.workout_insert(1, '2022-11-03 18:00:00');
