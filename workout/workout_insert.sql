40f8cec34a7af19e5e5fdfe5f1aa13508068f2f0
Semenov <semenov@mail.ru>
2022-11-05 21:52:32 +0400

    update function workout_insert and create workout_exercises_update

--- workout/workout_insert.sql
+++ workout/workout_insert.sql
@@ -2,7 +2,14 @@
 drop function public.workout_insert;
 /* drop function */
 
- CREATE OR REPLACE FUNCTION public.workout_insert(_id_user integer, _date timestamp without time zone DEFAULT CURRENT_TIMESTAMP, OUT id_ integer, OUT result_type_ result_type)
+ CREATE OR REPLACE FUNCTION public.workout_insert(
+	_id_user integer, 
+	_ids_exercises int4[] DEFAULT array[]::integer[],
+	_weight int4[] DEFAULT array[]::integer[], 
+	_date timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+	OUT id_ integer, 
+	OUT result_type_ result_type
+ )
  RETURNS record
  LANGUAGE plpgsql
 AS $function$
@@ -11,6 +18,8 @@ AS $function$
 		if result_type_.status_ <> 0 then 
 			insert into workout (id_user, "date") values (_id_user, _date) RETURNING id into id_;
 			select 1 as status_, null as error_ into result_type_ ;
+			insert into public.workout_exercises (id_workourt, id_exercises, weight) 
+				values (id_, unnest(_ids_exercises), unnest(_weight));
 			PERFORM public.log_insert(_id_user, 'Пользователь успешно создал тренировку id - ' || id_);
 		end if;
 	END;
---
+++ workout_exercises/workout_exercises_insert.sql
@@ -0,0 +1,20 @@
+ CREATE OR REPLACE FUNCTION public.workout_exercises_insert(
+	_id_user int4,
+	_id_workourt int4,
+	_ids_exercises int4[],
+	_weight int4[],
+	out result_type_ result_type
+	
+)
+ RETURNS result_type
+ LANGUAGE plpgsql
+AS $function$
+	begin
+		select * into result_type_ from public.workout_check(_id_workourt,_id_user);
+		if result_type_.status_ <> 0 then
+			insert into public.workout_exercises (id_workourt, id_exercises, weight) 
+			values (_id_workourt, unnest(_ids_exercises), unnest(_weight));
+            PERFORM public.log_insert(_id_user, 'Пользователь успешно изменил тренировку id - ' || id_);
+		end if;
+	END;
+$function$;
