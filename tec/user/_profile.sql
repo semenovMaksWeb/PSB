0fbfcfb0c2aacee5edf364b439451d19b597a333
Semenov <semenov@mail.ru>
2022-12-03 11:58:34 +0400

    PSB-1 Сущность user

--- tec/user/_profile.sql
+++ tec/user/_profile.sql
@@ -10,8 +10,8 @@ create type tec."user_get_profile" AS (
 )
 
 
-CREATE OR REPLACE FUNCTION tec.profile(_token varchar, out result_ public.result_type, out profile_ tec.user_get_profile)
- RETURNS RECORD
+CREATE OR REPLACE FUNCTION tec.profile(_token character varying, OUT result_ result_type, OUT profile_ tec.user_get_profile)
+ RETURNS record
  LANGUAGE plpgsql
 AS $function$
 	declare
@@ -22,7 +22,9 @@ AS $function$
 			from tec."token" t  
 			left join  tec."user" u on u.id  = t.id_user 
 			where t.value = _token and t.active = true and t.date_end > CURRENT_TIMESTAMP;
-        if user_profile.active = false then
+        if user_profile is null then
+        	select 0 as status_, 'Не валидный токен' as error_ into result_ ;
+		elseif user_profile.active = false then
             select 0 as status_, 'Ваш аккаунт заблокирован' as error_ into result_ ;
         elseif user_profile.confirmed = false then
             select 0 as status_, 'Вы не подвердили аккаунт через почту' as error_ into result_ ;
@@ -31,4 +33,5 @@ AS $function$
             profile_ = user_profile;
        end if;
     END;
-$function$;
+$function$
+;
