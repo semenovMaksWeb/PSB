da81ad112df90b3c0799373b09b9929aaca1d844
Semenov <semenov@mail.ru>
2022-12-06 09:51:13 +0400

    PSB-6 сущность right

---
+++ tec/right/check_right.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION tec.check_right(_token character varying, _right_const_name varchar, OUT result_ json)
+ RETURNS json 
+ LANGUAGE plpgsql
+AS $function$
+	declare
+		result_profile result_profile;
+        check_right_user boolean;
+    BEGIN
+       select * into result_profile from tec.profile(_token);
+       if ((result_profile.result_::json->'status_')::text::int) = 1 and result_profile.profile_::json->'id' is not null then
+            select exists (	
+                select r.id  from tec."right" r where r.const_name  = _right_const_name and r.id in (
+                    select  rig.id as rig_id from tec."user" u 
+                    left join tec.roles_user ru on ru.id_user = u.id 
+                    left join tec.roles r on r.id = ru.id_roles 
+                    left  join  tec.right_roles rr on rr.id_roles = ru.id_roles 
+                    left join tec."right" rig on rig.id = rr.id_right 
+                    where u.id  = (result_profile.profile_::json->'id')::text::int
+                )	
+            ) into check_right_user;
+       end if;
+      result_ := result_profile.result_;
+        if  check_right_user = false then
+            select * into result_  from public.get_result(0, 'У вас нет прав на данную операцию');
+        end if;
+    END;
+$function$
+;
