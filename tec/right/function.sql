947fe525036d8daa1cb713aa3d8ecac7b4ba0d42
Semenov <semenov@mail.ru>
2022-12-31 19:17:32 +0400

    PSB-6 сущность right

---
+++ tec/right/function.sql
@@ -0,0 +1,50 @@
+CREATE OR REPLACE FUNCTION tec.right_get_userid(_id integer)
+RETURNS TABLE(id integer)
+LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    RETURN query
+        select rr.id_right from  tec.user u
+            left join tec.roles_user ru on ru.id_user = u.id
+            left join tec.roles r on ru.id_roles = r.id
+            left join tec.right_roles rr on rr.id_roles = r.id 
+            where u.id = _id;
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION tec.right_get_userid_list(_id integer)
+RETURNS TABLE(id integer, name varchar, const_name varchar)
+LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    RETURN query
+       select rig.id, rig."name", rig.const_name  from  tec.user u
+            left join tec.roles_user ru on ru.id_user = u.id
+            left join tec.roles r on ru.id_roles = r.id
+            left join tec.right_roles rr on rr.id_roles = r.id
+            left join tec."right" rig on rig.id  = rr.id_right
+            where u.id = _id;
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION tec.right_get_not_userid_list(_id integer)
+RETURNS TABLE(id integer, name varchar, const_name varchar)
+LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    RETURN query
+        select r.id, r."name", r.const_name from tec."right" r where r.id not in (
+	        select rig.id from  tec.user u
+            left join tec.roles_user ru on ru.id_user = u.id
+            left join tec.roles r on ru.id_roles = r.id
+            left join tec.right_roles rr on rr.id_roles = r.id
+            left join tec."right" rig on rig.id  = rr.id_right
+            where u.id = _id and rig.id is not null
+        );
+    END;
+$function$;
+
+
+select * from tec.right_get_userid(18);
+select * from tec.right_get_userid_list(18);
+select * from tec.right_get_not_userid_list(18);
