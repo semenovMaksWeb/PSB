e959d7adb721294e795c8206be4d1e06289bc6ca
Semenov <semenov@mail.ru>
2023-01-01 11:59:46 +0400

    PSB-7 сущность user_roles

--- tec/roles_user/function.sql
+++ tec/roles_user/function.sql
@@ -35,9 +35,23 @@ AS $function$
     END;
 $function$;
 
+
+CREATE OR REPLACE FUNCTION tec.roles_user_insert(_id_user integer, _id_roles integer[])
+RETURNS boolean
+LANGUAGE plpgsql
+AS $function$
+    BEGIN
+        if (select * from tec.roles_user_check_not_roles(_id_user, _id_roles)) and (select * from tec.roles_check_ids(_id_roles)) then
+            return (select true);
+        END IF;
+        return (select false);
+    END;
+$function$;
+
+
 select * from tec.roles_user_get_name_id(18);
 select * from tec.roles_user_not_get_name_id(18);
 select * from tec.roles_user_check_not_roles(18, ARRAY [1]);
 
 DROP FUNCTION tec.roles_user_get_name_id;
-DROP FUNCTION tec.roles_user_not_get_name_id;
+DROP FUNCTION tec.roles_user_not_get_name_id;   
