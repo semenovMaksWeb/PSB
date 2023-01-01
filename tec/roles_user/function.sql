f8cd83134f184dedc660984372195ac203109eb0
Semenov <semenov@mail.ru>
2023-01-01 13:56:51 +0400

    PSB-7 сущность user_roles

--- tec/roles_user/function.sql
+++ tec/roles_user/function.sql
@@ -36,22 +36,27 @@ AS $function$
 $function$;
 
 
-CREATE OR REPLACE FUNCTION tec.roles_user_insert(_id_user integer, _id_roles integer[])
-RETURNS boolean
+CREATE OR REPLACE FUNCTION tec.roles_user_insert(_id_user integer, _id_roles integer[], out result_ json)
 LANGUAGE plpgsql
 AS $function$
     BEGIN
-        if (select * from tec.roles_user_check_not_roles(_id_user, _id_roles)) and (select * from tec.roles_check_ids(_id_roles)) then
-            return (select true);
-        END IF;
-        return (select false);
+        if (select * from tec.roles_user_check_not_roles(_id_user, _id_roles)) = false then
+            select * into result_ from public.get_result(0, 'Указанные роли уже есть у пользователя');
+            return;
+        elseif (select * from tec.roles_check_ids(_id_roles)) = false then
+            select * into result_ from public.get_result(0, 'Указанные роли не существуют');
+            return;
+        end if;
+        insert into tec.roles_user (id_user, id_roles) VALUES (_id_user, UNNEST(_id_roles));
+        select * into result_ from public.get_result(1, '');
     END;
-$function$;
+$function$
 
 
 select * from tec.roles_user_get_name_id(18);
 select * from tec.roles_user_not_get_name_id(18);
 select * from tec.roles_user_check_not_roles(18, ARRAY [1]);
+select  * from tec.roles_user_insert(18, ARRAY [1]);
 
 DROP FUNCTION tec.roles_user_get_name_id;
 DROP FUNCTION tec.roles_user_not_get_name_id;   
