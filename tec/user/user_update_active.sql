508d3551d7778b9ece9146db8464279b28113c63
Semenov <semenov@mail.ru>
2023-01-06 10:34:48 +0400

    PSB-1 Сущность user

--- tec/user/user_update_active.sql
+++ tec/user/user_update_active.sql
@@ -21,7 +21,7 @@ AS $function$
     	end if;
 
 	   	if result_type_.status_ <> 0 then 
-	   	    update tec."user" set confirmed = true;
+	   	    update tec."user" set confirmed = true where id = _id_user;
 			select 1 as status_, null as error_ into result_type_ ;
     		PERFORM tec.log_insert(_id_user, 'Пользователь успешно активирован');
 		end if;
